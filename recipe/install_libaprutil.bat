f92590bf58b406ae69b32b5452a5d77f3fbc9f94
Ivan Iziumov <iiziumov@anaconda.com>
2026-05-06 14:23:53 +0200

    apr 1.7.6

--- recipe/install_libaprutil.bat
+++ recipe/install_libaprutil.bat
@@ -4,7 +4,8 @@ cmake -GNinja ^
     -S%SRC_DIR%\%SUBPKG% ^
     -B%SRC_DIR%\%SUBPKG%.build ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 if errorlevel 1 exit 1
 
 cmake --build %SRC_DIR%\%SUBPKG%.build -- install
